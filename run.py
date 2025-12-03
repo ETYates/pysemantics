30ae49fa4bf8702cc7b8475c41827a0f030eaa52
Ethan Yates <ethan@Ethan-SAGER>
2025-12-03 11:00:45 -0500

    Refinement of grammar and assignment of categories.

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
+"""
+run - file to run the whole program
+
+This module is for running the whole semantics program. SpaCy is used for
+tokenization and a top-down MG[+SpIC_mrg] parser is used for constituency
+parsing.
+"""
 import spacy
 import pprint
 from mgtdbp import parse, dt2t, pptree
@@ -7,7 +14,10 @@ doc = nlp("some text")
 
 
 class Lexeme:
-
+    """A simple class to carry data from spacy into the MG parser. There is
+    also a function to convert tags from the standard PENN treebank tags to the
+    simple tags used by Edward Stabler et al. in their scholarship for this
+    formalism"""
 
     def __init__(self, text: str, tag: str, lemma: str):
         self.text = text
@@ -19,15 +29,20 @@ class Lexeme:
         return f"{self.lemma}: {self.cat}"
 
     def set_cat(self) -> None:
+        """Convert PENN treebank tags to simple tags."""
         match self.tag:
             case "JJ" | "JJR" | "JJS":
                 self.cat = [('sel', 'n'),('cat', "n")]
             case "NN" | "NNS":
                 self.cat = [('cat', "n")]
             case "NNP" | "PRP" | "NNPS":
-                self.cat = [('cat', 'd')]
+                self.cat = [[('cat', 'd')],
+                            [('cat', 'd'),('neg', 'case')]
+                            ]
             case "DT":
-                self.cat = [('sel', 'n'),('cat', "d")]
+                self.cat =[[('sel', 'n'),('cat', "d")],
+                           [('sel', 'n'),('cat', "d"),('neg', 'case')]
+                           ]
             case "VBZ" | "VBD" | "VBP" | "VBN" | "VBG" | "VB":
                 match self.lemma:
                     case "do":
@@ -37,7 +52,9 @@ class Lexeme:
                     case "be":
                         self.cat = [('sel', 'v'),('cat', 'b')]
                     case _:
-                        self.cat = [('cat','v')]
+                        self.cat = [[('sel', 'd'),('sel', 'd'),('cat','v')],
+                                    [('sel', 'c'),('sel', 'd'),('cat','v')]
+                                    ]
             case ".":
                 self.cat = [('cat', 's')]
             case "RB" | "RBR" | "RBS":
@@ -49,29 +66,44 @@ class Lexeme:
             case "EX":
                 self.cat = [('cat', 'x')]
             case "WP":
-                self.cat = [('cat', 'w')]
+                self.cat = [('cat', 'd'), ('neg', 'wh')]
             case "WDT":
                 self.cat = [('sel', 'n'), ('cat', 'd'), ('neg', 'wh')]
             case _:
                 raise Exception(f"POS tag {self.tag} is unimplemented.")
 
 class Lexicon:
+    """
+    This class contains a lexicon that starts with a small number of lexical
+    entries (epsilons) and incrementally adds new entries as input text is
+    processed.
+    """
 
     def __init__(self):
-        self.lexicon = [([], [('sel', 'v'),('cat','c')]),
-                   ([],[('sel','v'),('pos','wh'), ('cat', 'c')])
-                   ]
-        self.members: set[str] = {}
+        # base lexical entries for grammar
+        self.lexicon = [([],[('sel','v'),('cat','c')]),
+                        ([],[('sel','v'),('pos','wh'),('cat','c')]),
+                        ([],[('sel', 't'),('cat', 'c')]),
+                        ([],[('sel', 't'),('pos', 'case'),('cat', 'c')]),
+                        ([],[('sel', 't'),('pos', 'wh'),('cat', 'c')])]
+        self.members = set() # Keeps track of which words are in lexicon.
 
     def add(self, text, cat):
-        entry = ([text], cat)
-        self.lexicon.append(entry)
+        if isinstance(cat,list) and all(isinstance(f, list) for f in cat):
+            for f in cat:
+                entry = ([text], f)
+                self.lexicon.append(entry)
+        else:
+            entry = ([text], cat)
+            self.lexicon.append(entry)
+
 
     def __str__(self):
         return str(self.lexicon)
 
 
 def tokenize(raw_input: str):
+    # process input with spacy
     lexes: list = []
     doc = nlp(raw_input)
     for item in doc:
@@ -86,18 +118,15 @@ def __main__():
     lexicon = Lexicon()
     while (raw_input := input("|- ")) != 'quit':
         lexes = tokenize(raw_input)
+        inpt = []
         for lex in lexes:
-            text = lex.text
             cat = lex.cat
+            text = f"{lex.text}: {lex.lemma}"
+            inpt.append(text)
             if text not in lexicon.members:
-                if cat == [('cat', 'v')]:
-                    lexicon.add(text, [('sel', 'd'), ('sel', 'd'), ('cat', 'v')])
-                    lexicon.add(text, [('sel', 'c'), ('sel', 'd'), ('cat', 'v')])
-                    lexicon.members[text] = True
-                else:
-                    lexicon.add(text, cat)
-        inpt = raw_input.split()
-        dt = parse(lexicon.lexicon, 'c', -1 * float(0.001), inpt)
+                lexicon.add(text, cat)
+                lexicon.members.add(text)
+        dt = parse(lexicon.lexicon, 'c', -1 * float(0.0000001), inpt)
         pptree(dt2t(dt))
         pprint.pprint(lexicon.lexicon)
 
