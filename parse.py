8a242e7136fb0a9d971e73671a219a1c7760c6e2
ETYates <ethan.t.yates@gmail.com>
2026-03-07 13:30:29 -0500

    added docstring comments and removed unused methods

--- parse.py
+++ parse.py
@@ -1,12 +1,39 @@
-from nltk.grammar import FeatureGrammar, Production, FeatStructNonterminal
+# repository: pysemantics
+# Author: Ethan Yates <ethan.t.yates@gmail.com>
+
+from nltk.grammar import FeatureGrammar, FeatStructNonterminal
 from nltk.parse.featurechart import FeatureChartParser
-from nltk.data import load
-from nltk.parse import load_parser
 from collections import defaultdict
 import spacy
 
+"""
+Custom classes made to wrap around nltk functions. The Grammar class takes an
+initially specified grammar in the form of a string and stores the information
+in a dictionary. The parser contains the grammar inside its own object
+instantiation, which is updated upon every call. This process allows for
+maximum coverage with minimum specification.
+"""
 
 class Grammar:
+    """
+    Represents the grammar to be used by the system. A minimal grammar is
+    first specified in Grammar.basis as the fundamental behavior of the
+    parser. 
+
+    Only nonterminals are specified in this grammar--all terminal
+    nodes (except traces) are only added after being parsed from input
+    sentences. 
+
+    The basis is specified in an initial string that is read
+    into a dictionary data structure. Then, after every input, the grammar
+    will be updated with the appropriate rules of the form NonTerm -> Term
+    in order to capture the input as specified by the input lexemes. 
+
+    The grammmar is updated by added new entries to the dictionary. This
+    dictionary, every time it is updated, is then exported to a string
+    which is then passed to the ready-made nltk functions to build a
+    grammar object and a parser.
+    """
 
     start = 'CP'
 
@@ -57,9 +84,17 @@ class Grammar:
         self._rules = defaultdict(set)
         self._nonterminals: set[str] = set()
 
-        self._read_basis()
+        self._read_basis() # loads the base grammar
 
     def update(self, tokens: list[str], tags: list[str]) -> None:
+        """
+        Given a list of tokens and tags, process each respective token and tag
+        into a production rule, and add it to the grammar.
+
+        :param tokens: list of tokens representing the sentence. 
+        :param tags: list of tags representing the nonterminal
+            symbols for adding words to production rules
+        """
         for tag, token in zip(tags, tokens):
             terminal = f"'{token}'"
             if token in self.copula:
@@ -67,6 +102,14 @@ class Grammar:
             self._add_rule(tag, terminal)
 
     def export_parser(self) -> FeatureChartParser:
+        """
+        Exports the grammar into a string and then passes the string
+        to nltk.grammar.fromstring(). This returns a chart parser that
+        is used to parser the next input.
+
+        :returns parser: the nltk ChartParser that is used to parse
+            input sentences
+        """
         grammar_str = self.export_str()
         feature_grammar = FeatureGrammar.fromstring(grammar_str)
         parser = FeatureChartParser(feature_grammar)
@@ -74,6 +117,10 @@ class Grammar:
         return parser
 
     def export_str(self) -> str:
+        """
+        Export the entries in the grammar dictionary to string form. This
+        string is readable by nltk modules such as Grammar.fromstring()
+        """
         output: list[str] = []
         output.append(f"% start {self.start}")
         for key in self._rules:
@@ -89,6 +136,10 @@ class Grammar:
         return out_str
 
     def _read_basis(self) -> None:
+        """
+        Read the initial string containing the base grammar specification and
+        save the data into a dictionary.
+        """
         lines = self.basis.strip().split('\n')
         basis = [line.strip() for line in lines]
         symbol = None
@@ -117,24 +168,34 @@ class Grammar:
                     raise ValueError("Invalid basis.")
 
     def _add_rule(self, cat: str, token: str) -> None:
+        """
+        Add a single rule to the grammar.
+
+        :param cat: grammatical category and non-term symbol
+        :param token: the string of the word to be added as a
+            terminal symbol
+        """
         self._rules[cat].add((token,))
         
     def _check_symbol(self, symbol: str) -> str | FeatStructNonterminal:
+        """Checks a given symbol string against a list containing all the
+        non-terminal symbols in the grammar. 
+
+        :param symbol: the input symbol
+        """
         if symbol in self._nonterminals:
             return FeatStructNonterminal(symbol)
 
         return symbol
 
-    def _make_production(self, cat: str, product: tuple[str] | tuple[str, str]) -> Production:
-        left = FeatStructNonterminal(cat)
-        right = tuple(self._check_symbol(symbol) for symbol in product)
-
-        production = Production(left, right)
-        return production
-
-
 
 class Parser:
+    """
+    A wrapper class for an nltk FeatureChartParser. Contains a grammar,
+    a parser, and a spacy object for performing the initial tokenization
+    and tagging. Persistently updates the grammar from the new input,
+    based on the tags and tokens in the spacy output.
+    """
 
     def __init__(self):
         self._grammar = Grammar()
@@ -142,6 +203,13 @@ class Parser:
         self._parser = self._grammar.export_parser()
 
     def parse(self, raw_input: str):
+        """
+        Takes an unprocessed string and processes it with spacy, adds the 
+        produced lexical entries into the grammar, and uses the grammar
+        to parse the input tokens.
+
+        :param raw_input: a string provided by the user
+        """
 
         words = self._nlp(raw_input)
         tokens: list[str] = []
@@ -173,14 +241,3 @@ class Parser:
         trees = self._parser.parse(tokens)
 
         return trees, lemmas
-
-    def repl(self):
-
-        while ((raw_input := input('|- ')) != 'quit'):
-            trees = self.parse(raw_input)
-            for tree in trees:
-                print(tree)
-
-if __name__ == "__main__":
-    parser = Parser()
-    parser.repl()
